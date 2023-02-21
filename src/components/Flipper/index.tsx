afa52ebfc9be2374cfbe3ea1c57ff7d83662a1b9
Michelle Berger <bergerm613@gmail.com>
2023-02-20 21:24:56 -0500

    add animation to number changes

--- src/components/Flipper/index.tsx
+++ src/components/Flipper/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import * as React from "react";
 
 import "./styles.css";
@@ -10,9 +11,25 @@ type FlipperProps = {
 export const Flipper = ({ amount, subtitle }: FlipperProps) => {
   return (
     <div className="flipper-block">
+      <h3>{subtitle}</h3>
+
       <div className="amount-box top">{amount}</div>
       <div className="amount-box bottom">{amount}</div>
-      <h3>{subtitle}</h3>
+
+      <motion.div
+        className="amount-box bottom flip"
+        key={amount}
+        initial={{ y: -125 }}
+        animate={{
+          rotateX: 180,
+          y: -240,
+          background: "#562c2c",
+          opacity: [100, 100, 100, 0],
+        }}
+        transition={{ duration: 0.1, default: { ease: "linear" } }}
+      >
+        {amount}
+      </motion.div>
     </div>
   );
 };
