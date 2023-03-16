89443cbb514bb9cad7f097653cd4cc31909c6dd5
Michelle Berger <bergerm613@gmail.com>
2023-03-15 21:38:59 -0400

    converting into wedding countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
 
 import { Flipper } from "./components/Flipper";
 import { isCountdownDone } from "./utils";
 import Confetti from "react-confetti";
 import { useCountdown } from "./hooks/useCountdown";
-import { getHebcalSunset } from "./utils/apis";
-
-import * as ReactGA from "react-ga";
 
 import "./styles.css";
 
-const TRACKING_ID = "G-RY476C5WV6";
-ReactGA.initialize(TRACKING_ID);
+const weddingDate = new Date(2023, 3, 19, 5, 0); // April 19, 2023 - 6:00 PM
 
 const App = () => {
-  const [purimStartDate, setPurimStartDate] = useState<Date>();
-  const { days, hours, minutes, seconds } = useCountdown(purimStartDate);
-
-  useEffect(() => {
-    const geoname = "5128581"; //geoname of New York City
-
-    getHebcalSunset(geoname).then((sunset) => {
-      setPurimStartDate(sunset);
-    });
-  }, []);
-
+  const { days, hours, minutes, seconds } = useCountdown(weddingDate);
   return (
     <>
       {isCountdownDone(days, hours, minutes, seconds) && <Confetti />}
 
       <div className="countdown-container">
-        <h1 className="title">Purim 2023 Let's Goooo</h1>
+        <h1 className="title">Leah and Naftali Wedding Countdown!!</h1>
 
         <div className="flippers-container">
           <Flipper amount={days} subtitle={"days"} />
